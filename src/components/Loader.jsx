7a57607775bafae2b8baa8e14af5a09c1ea956ea
blockdev09 <135862086+blockdev09@users.noreply.github.com>
2024-06-28 11:19:22 +0530

    Loader Added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-
+import videoloader from '../assets/videos/video.mp4'
 const Loader = () => {
   // return <div>Loading...</div>;
   return (
-    <div className="loadercontainer">
-      <div className="ring"></div>
-      <div className="ring"></div>
-      <div className="ring"></div>
- <span className="loadingtext">Loading...</span>
-    </div>
+//     <div className="loadercontainer">
+//       <div className="ring"></div>
+//       <div className="ring"></div>
+//       <div className="ring"></div>
+//  <span className="loadingtext">Loading...</span>
+//     </div>
+   <div className="videoloader">
+     <video autoPlay loop muted src={videoloader} />
+   </div>
   )
 };
 
